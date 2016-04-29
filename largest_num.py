af385bde4dd80e93eaf3e58944208c62fea8b724
Hackbright Student <info@hackbrightacademy.com>
2016-04-28 20:47:44 -0700

    Lab exercises

---
+++ largest_num.py
@@ -0,0 +1,9 @@
+lugage = int(raw_input("How much doues you lugage weigh, prompt enter a number"))
+print type(lugage)
+
+def weigh (lugage):
+    if lugage > 50: 
+        print "There is a $25 charge for your suitcase"
+    else:
+        print "no charge"
+weigh (lugage)
